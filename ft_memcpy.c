576071850a6fe7c4a1715a3f7b58b9eebf51d9b9
Biraj <finepine248@gmail.com>
2024-10-16 12:43:58 +0000

    Add

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -7,6 +7,8 @@ void *memcpy(void *dest_str, const void *src_str, size_t n)
     char *d;
     const char *s;
 
+    if (dest_str == '\0' && src_str == '\0')
+        return (NULL);
     i = 0;
     d = (char *)dest_str;
     s = (const char *)src_str;
---
+++ ft_memmove.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+#include <stdio.h>
+
+void *memmove(void *dest_str, const void *src_str, size_t numBytes)
+{
+    
+}
