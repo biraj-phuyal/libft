e88253684e2c097b981b5183051eeca6dda2ef5e
biraj-phuyal <birajphuyal11@gmail.com>
2025-04-11 15:55:00 +0100

    Forgot to save

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:36:50 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/03/03 18:36:51 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:51:12 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 #include "libft.h"
 #include <stdio.h>
 
+int ft_isdigit(int c);
+
 int ft_isalnum(int ch)
 {
-    if (ch >= 0 && ch <= 9)
+    if (ft_isdigit(ch))
         return (1);
     if (ch >= 'A' && ch <= 'Z')
         return (1);
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:37:14 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/04/11 15:47:15 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:51:18 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@
 
 int ft_isdigit(int c)
 {
-    if (c >= 0 && c <= 9)
-        return (1);
-    return (0);
+    return (c >= '0' && c <= '9');
 }
 /* 
 int main()
