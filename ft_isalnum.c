aae2696e5d65ae2d2ee4b2cfff1b526f5f39b2fa
biraj-phuyal <birajphuyal11@gmail.com>
2025-04-11 16:08:17 +0100

    fix

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:36:50 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/04/11 16:06:50 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/04/11 16:08:11 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int ft_isdigit(int c);
 
 int ft_isalnum(int ch)
 {
-    return (ft_isdigit(ch)) || ft_isdigit(ch);
+    return (ft_isdigit(ch) || ft_isdigit(ch));
 }
 /* 
 int main()
