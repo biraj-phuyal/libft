19c8b64a390dd01dbcfa3d2f70590827d5b81de8
biraj-phuyal <birajphuyal11@gmail.com>
2025-04-11 15:51:29 +0100

    Fixed isdigit, isalnum and strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,14 +6,14 @@
 /*   By: biphuyal <biphuyal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 10:32:57 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/04/05 10:32:58 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:47:17 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int ft_strlen(const char *str)
 {
     int i;
 
