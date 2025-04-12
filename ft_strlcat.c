14d25d3507f58716546c9420d025421a9a2d865b
biraj-phuyal <birajphuyal11@gmail.com>
2025-04-12 07:30:31 +0100

    Fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,14 @@
 /*   By: biphuyal <biphuyal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 10:32:43 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/04/11 14:24:43 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/04/11 21:05:05 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "libft.h"
+
+int ft_strlen(const char *s);
 
 unsigned int ft_strlcat(char *dest, const char *src, unsigned int n)
 {
@@ -19,13 +21,14 @@ unsigned int ft_strlcat(char *dest, const char *src, unsigned int n)
     unsigned int d_len;
     unsigned int i;
 
-    s_len = 0;
+    s_len = ft_strlen(src);
     i = 0;
     d_len = 0;
-    while (src[s_len])
-        s_len++;
-    while (dest[d_len])
-        d_len++;
+    if (dest)
+    {
+        while (d_len < n && dest[d_len])
+            d_len++;
+    }
     if (d_len >= n)
         return (n + s_len);
     while (d_len + i + 1 < n && src[i])
